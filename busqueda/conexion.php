0bbe74320498df098497100fcc0ea583e623f345
Jhan Isaac Castillo Tuesta <itzcadilac16@gmail.com>
2022-12-28 22:25:53 -0500

    Update conexion.php

--- busqueda/conexion.php
+++ busqueda/conexion.php
@@ -1,8 +1,8 @@
 <?php 
 $bd_host = "localhost"; 
-$bd_usuario = "novoex_asesores"; 
-$bd_password = "casa123456"; 
-$bd_base = "novoex_sstasesores"; 
+$bd_usuario = "sstase_consul"; 
+$bd_password = "sstconsultas2020"; 
+$bd_base = "sstase_consultas"; 
 //$con = mysql_connect($bd_host, $bd_usuario, $bd_password); 
 $con = mysqli_connect($bd_host, $bd_usuario, $bd_password, $bd_base); 
 //mysql_select_db($bd_base, $con); 
